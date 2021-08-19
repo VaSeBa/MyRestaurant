77d488485fcf5ff45abab5a7c6f778ee6111e0de
VaSeBa <badasin.valentin@yandex.ru>
2021-08-19 07:44:00 +0300

    Initial commit

---
+++ src/main/java/ru/vaseba/myrestaurant/MyRestaurantApplication.java
@@ -0,0 +1,13 @@
+package ru.vaseba.myrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRestaurantApplication.class, args);
+    }
+
+}
